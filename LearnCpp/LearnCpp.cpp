56cf84d9ef32bd5e9536b975a4d8e179bc862d17
gatordan82 <gatordan82@gmail.com>
2016-06-05 21:54:25 -0400

    First Hello World! program, with interrupt to keep console window open.

--- LearnCpp/LearnCpp.cpp
+++ LearnCpp/LearnCpp.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
 
 int main()
 {
+	std::cout << "Hello World!" << std::endl;
+
+	std::cin.clear();
+	std::cin.ignore(32767, '\n');
+	std::cin.get();
+
     return 0;
 }
 
